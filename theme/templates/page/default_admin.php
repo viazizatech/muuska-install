771e263ce20873c7bddf991b42c2269e4d99a3fe
Jonathan NGUIDJO <50013726+Nguidjo@users.noreply.github.com>
2022-01-18 18:49:22 +0100

    282 changes files
    282 changes files

--- theme/templates/page/default_admin.php
+++ theme/templates/page/default_admin.php
@@ -55,24 +55,76 @@
                 			-->
 						</div>
 					</div>
+					<div class="kt-aside-menu-wrapper kt-grid__item kt-grid__item--fluid" id="kt_aside_menu_wrapper">
+						<div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500">
+							<ul class="kt-menu__nav ">
+								<li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<rect x="0" y="0" width="24" height="24" />
+													<path d="M2.56066017,10.6819805 L4.68198052,8.56066017 C5.26776695,7.97487373 6.21751442,7.97487373 6.80330086,8.56066017 L8.9246212,10.6819805 C9.51040764,11.267767 9.51040764,12.2175144 8.9246212,12.8033009 L6.80330086,14.9246212 C6.21751442,15.5104076 5.26776695,15.5104076 4.68198052,14.9246212 L2.56066017,12.8033009 C1.97487373,12.2175144 1.97487373,11.267767 2.56066017,10.6819805 Z M14.5606602,10.6819805 L16.6819805,8.56066017 C17.267767,7.97487373 18.2175144,7.97487373 18.8033009,8.56066017 L20.9246212,10.6819805 C21.5104076,11.267767 21.5104076,12.2175144 20.9246212,12.8033009 L18.8033009,14.9246212 C18.2175144,15.5104076 17.267767,15.5104076 16.6819805,14.9246212 L14.5606602,12.8033009 C13.9748737,12.2175144 13.9748737,11.267767 14.5606602,10.6819805 Z" fill="#000000" opacity="0.3" />
+													<path d="M8.56066017,16.6819805 L10.6819805,14.5606602 C11.267767,13.9748737 12.2175144,13.9748737 12.8033009,14.5606602 L14.9246212,16.6819805 C15.5104076,17.267767 15.5104076,18.2175144 14.9246212,18.8033009 L12.8033009,20.9246212 C12.2175144,21.5104076 11.267767,21.5104076 10.6819805,20.9246212 L8.56066017,18.8033009 C7.97487373,18.2175144 7.97487373,17.267767 8.56066017,16.6819805 Z M8.56066017,4.68198052 L10.6819805,2.56066017 C11.267767,1.97487373 12.2175144,1.97487373 12.8033009,2.56066017 L14.9246212,4.68198052 C15.5104076,5.26776695 15.5104076,6.21751442 14.9246212,6.80330086 L12.8033009,8.9246212 C12.2175144,9.51040764 11.267767,9.51040764 10.6819805,8.9246212 L8.56066017,6.80330086 C7.97487373,6.21751442 7.97487373,5.26776695 8.56066017,4.68198052 Z" fill="#000000" />
+												</g>
+											</svg></span><span class="kt-menu__link-text">info Association</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
+									<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
+										<ul class="kt-menu__subnav">
+											<li class="kt-menu__item  kt-menu__item--parent" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text">association</span></span></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="association" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">info</span></a></li>
+					
+										</ul>
+									</div>
+								</li>																						
+								<li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><span class="kt-menu__link-icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+												<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+													<rect x="0" y="0" width="24" height="24" />
+													<path d="M3.5,21 L20.5,21 C21.3284271,21 22,20.3284271 22,19.5 L22,8.5 C22,7.67157288 21.3284271,7 20.5,7 L10,7 L7.43933983,4.43933983 C7.15803526,4.15803526 6.77650439,4 6.37867966,4 L3.5,4 C2.67157288,4 2,4.67157288 2,5.5 L2,19.5 C2,20.3284271 2.67157288,21 3.5,21 Z" fill="#000000" opacity="0.3" />
+													<polygon fill="#000000" opacity="0.3" points="4 19 10 11 16 19" />
+													<polygon fill="#000000" points="11 19 15 14 19 19" />
+													<path d="M18,12 C18.8284271,12 19.5,11.3284271 19.5,10.5 C19.5,9.67157288 18.8284271,9 18,9 C17.1715729,9 16.5,9.67157288 16.5,10.5 C16.5,11.3284271 17.1715729,12 18,12 Z" fill="#000000" opacity="0.3" />
+												</g>
+											</svg></span><span class="kt-menu__link-text">AssoManager</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
+									<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
+										<ul class="kt-menu__subnav">
+											<li class="kt-menu__item  kt-menu__item--parent" data-ktmenu-submenu-toggle="hover" aria-haspopup="true"><span class="kt-menu__link"><span class="kt-menu__link-text"></span></span></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/membre" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Membres</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/epargne" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Epargne</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/emprunt" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Emprunts</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/sanction" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Santions</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/interet" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Interets</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/fond" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Fonds</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/souscrire" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Souscrire</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/timbre" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Timbres</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/annonce" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Annonces</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/exercice" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Exercices</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/seance" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text"> Seances</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/tontine"class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Tontine</span></a></li>
+											<li class="kt-menu__item " aria-haspopup="true"><a href="/app/admin/en/etre_membre" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">etre Membre</span></a></li>
+										</ul>
+									</div>
+								</li>										
+							</ul>
+						</div>
+					</div>
+
+
 					<?php echo $item->renderMainNav($globalConfig, $callerConfig, '<!-- begin:: Aside Menu --><div class="kt-aside-menu-wrapper kt-grid__item kt-grid__item--fluid" id="kt_aside_menu_wrapper"><div id="kt_aside_menu" class="kt-aside-menu " data-ktmenu-vertical="1" data-ktmenu-scroll="1" data-ktmenu-dropdown-timeout="500">', '</div></div><!-- End:: Aside Menu -->'); ?>
 					<!-- end:: Aside -->
 					</div>
-				
+					
 				<!-- end:: Aside -->
 				<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
-
+					
 					<!-- begin:: Header -->
 					<div id="kt_header" class="kt-header kt-grid__item  kt-header--fixed ">
-
 						<!-- begin:: Header Menu -->
                             <!-- Uncomment this to display the close button of the panel
                                 <button class="kt-header-menu-wrapper-close" id="kt_header_menu_mobile_close_btn"><i class="la la-close"></i></button>
                             -->
-                        	<div class="kt-header-menu-wrapper" id="kt_header_menu_wrapper">
+                        	<div class="kt-header-menu-wrapper text-right" id="kt_header_menu_wrapper">
                         		<?php echo $item->drawContentsByPosition('header_left', $globalConfig, $callerConfig, '<div id="kt_header_menu" class="kt-header-menu kt-header-menu-mobile  kt-header-menu--layout-default ">', '</div>'); ?>
                     			<?php echo $item->drawContentsByPosition('header_right', $globalConfig, $callerConfig, '<div class="kt-header__topbar">', '</div>'); ?>
-                        	</div>
+							</div>
+							<!--Log out button-->
+							<a href="logout" class="btn btn-secondary ml-auto" style="line-height: 50px">Log out</a>
 					</div>
 					<!-- end:: Header -->
 					<div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
